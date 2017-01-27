48cd2b968fc096249c51a48148f11413781cd7f3
Jacob <pber3099@gmail.com>
2017-01-27 12:57:24 -0500

    Cleanup and optimizations

--- island/assets/JSNativeComponents.js
+++ island/assets/JSNativeComponents.js
@@ -15,19 +15,27 @@ var NativeComponentDefaults = {
 	},
 	'n-sphere-collider': {
 		isTrigger: false,
-		center: { 'x':0, 'y': 0, 'x': 0 },
+		center: {
+			'x': 0, 'y': 0, 'x': 0
+		},
 		radius: 0,
 		type: 'environment'
 	},
 	'n-box-collider': {
 		isTrigger: false,
-		center: { 'x':0, 'y': 0, 'x': 0 },
-		size: { 'x':0, 'y': 0, 'x': 0 },
+		center: {
+			'x': 0, 'y': 0, 'x': 0
+		},
+		size: { 
+			'x': 0, 'y': 0, 'x': 0
+		},
 		type: 'environment'
 	},
 	'n-capsule-collider': {
 		isTrigger: false,
-		center: { 'x':0, 'y': 0, 'x': 0 },
+		center: { 
+			'x': 0, 'y': 0, 'x': 0
+		},
 		radius: 0,
 		height: 0,
 		direction: 'y',
@@ -54,76 +62,115 @@ var NativeComponentDefaults = {
 		minDistance: 1,
 		maxDistance: 12,
 	},
+	'n-browser': {
+		url: 'http://www.google.com/',
+		controls: 'hidden'
+	},
 };
 
-var NativeComponent = function (name, data, _mesh) {
-	this.name = name || 'n-object';
+var NativeComponent = function (name, data, _object) {
+	this.name = name.toLowerCase() || 'n-object';
 	this.data = data || null;
 	this.inClient = (altspace && altspace.inClient);
 
-	
 	if(NativeComponentDefaults[this.name]) {
-		this.data = Object.assign(NativeComponentDefaults[this.name], this.data);
+		this.data = Object.assign({}, NativeComponentDefaults[this.name], this.data);
 	}
 	
-	if(_mesh && typeof _mesh === 'object') {
-		this.mesh = _mesh;
-	}
-	
-	return this.init();
-};
-
-NativeComponent.prototype.init = function() {
-	if(!this.mesh) {
+	if(_object && (_object instanceof THREE.Object3D || _object instanceof THREE.Mesh || _object instanceof THREE.Group)) {
+		this.object = _object;
+	} else {
 		var geometry = new THREE.BoxGeometry(1, 1, 1);
 		var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-		material.visible = false;
+		material.visible = (this.data && this.data.visible) ? true : false;
 
-		this.mesh = new THREE.Mesh(geometry, material);
+		this.object = new THREE.Mesh(geometry, material);
 	}
-
-	this.mesh.userData.altspace = this.mesh.userData.altspace || {};
-	this.mesh.userData.altspace.collider = this.mesh.userData.altspace.collider || {};
-	this.mesh.userData.altspace.collider.enabled = false;
 	
-	if(this.inClient) altspace.addNativeComponent(this.mesh, this.name);
-
-	if(this.data) {
-		this.update(this.data);
+	switch(this.name) {
+		case 'n-mesh-collider':
+			var NC = this;
+			
+			this.object.traverse(function (childObj) {
+				if (childObj instanceof THREE.Mesh) {
+					NC.init(childObj);
+				}
+			});
+			return this;
+		break;
+		case 'n-sound':
+			var src = this.data.src;
+			if (src && !src.startsWith('http')) {
+				if (src.startsWith('/')) {
+					this.data.src = location.origin + src;
+				}
+				else {
+					var currPath = location.pathname;
+					if (!currPath.endsWith('/')) {
+						currPath = location.pathname.split('/').slice(0, -1).join('/') + '/';
+					}
+					this.data.src = location.origin + currPath + src;
+				}
+			}
+			
+			return this.init(this.object);
+		break;
+		default:
+			return this.init(this.object);
 	}
+};
+
+NativeComponent.prototype.init = function(object) {
+	object.userData.altspace = object.userData.altspace || {};
+	object.userData.altspace.collider = object.userData.altspace.collider || {};
+	object.userData.altspace.collider.enabled = false;
+	
+	if(this.inClient) altspace.addNativeComponent(object, this.name);
+	if(this.data) altspace.updateNativeComponent(object, this.name, this.data);
 	
 	return this;
 };
 
 NativeComponent.prototype.remove = function(andMesh) {
-	if(this.inClient) altspace.removeNativeComponent(this.mesh, this.name);
-	
-	if(andMesh) this.mesh.parent.remove(this.mesh);
-	
+	if(this.object) {
+		if(this.inClient) altspace.removeNativeComponent(this.object, this.name);
+		
+		if(andMesh) this.object.parent.remove(this.object);
+	}
 	return this;
 };
 
-NativeComponent.prototype.update = function(oldData, callback) {
-	this.data = Object.assign(this.data, oldData);
-	if(this.inClient) altspace.updateNativeComponent(this.mesh, this.name, this.data );
+NativeComponent.prototype.update = function(newData, callback) {
+	if(this.object) {
+		this.data = Object.assign({}, this.data, newData);
+		if(this.inClient) altspace.updateNativeComponent(this.object, this.name, this.data );
+		
+		callback && callback(this.object);
+	}
+	return this;
+};
+
+NativeComponent.prototype.call = function(functionName, functionArguments, callback) {
+	if(this.object) {
+		if(this.inClient) altspace.callNativeComponent(this.object, this.name, functionName, functionArguments);
+		
+		callback && callback(this.object);
+	}
+	return this;
+};
+
+NativeComponent.prototype.getObject = function(callback) {
+	if(!this.object) return false;
 	
-	callback && callback(this.mesh);
-	return this;
-};
-
-NativeComponent.prototype.call = function(functionName, functionArguments) {
-	if(this.inClient) altspace.callNativeComponent(this.mesh, this.name, functionName, functionArguments);
-	return this;
-};
-
-NativeComponent.prototype.getMesh = function(callback) {
-	callback && callback(this.mesh);
-	return this.mesh;
+	callback && callback(this.object);
+	return this.object;
 };
 
 NativeComponent.prototype.addTo = function(parent, callback) {
-	parent.add(this.mesh);
-	
-	callback && callback(this.mesh);
+	if(this.object) {
+		parent.add(this.object);
+		
+		callback && callback(this.object);
+	}
 	return this;
 };
---
+++ island/min/groupsConfig.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
+
+/** 
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ *
+ * See http://code.google.com/p/minify/wiki/CustomSource for other ideas
+ **/
+
+return array(
+    'js' => array('//assets/jquery.min.js', '//assets/aframe.min.js', '//assets/aframe-altspace-component.js', '//assets/altspace.min.js')
+);
